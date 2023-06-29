fb37965be7ef56d1f95d7041e1eb42bca51c5e24
Jamshid <murodovjamshid347@gmail.com>
2023-06-29 19:11:10 +0500

    first_task

---
+++ script.js
@@ -0,0 +1,20 @@
+var modal = prompt("First name")
+console.log("My name is " + modal);
+var modal = prompt("Last name")
+console.log("My last name " + modal);
+var modal = prompt("Your email")
+console.log("My email " + modal);
+var modal = prompt("Password")
+console.log("My password " + modal);
+var modal = prompt("phone number")
+console.log("My phone number " + modal);
+var modal = prompt("Robot emasligingizni tasdiqlang " + " 6+9")
+console.log("Robot emasligingizni tasdiqlang: 6+9 = " + (6 + 9) + " Sizning javob " + modal)
+var modal = prompt("Robot emasligingizni tasdiqlang " + " 56+9")
+console.log("Robot emasligingizni tasdiqlang: 56+9 = " + (56 + 9) + " Sizning javob " + modal)
+var modal = prompt("Robot emasligingizni tasdiqlang " + " 66-39")
+console.log("Robot emasligingizni tasdiqlang: 66-39 = " + (66 - 9) + " Sizning javob " + modal)
+var modal = prompt("Robot emasligingizni tasdiqlang " + " 89+63")
+console.log("Robot emasligingizni tasdiqlang: 89+63 = " + (89 + 63) + " Sizning javob " + modal)
+
+
